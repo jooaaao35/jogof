5dea2adc8fd186dab75e052d518e610f7ffbc11e
jooaaao35 <joaof0666@gmail.com>
2025-06-27 12:14:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('Bem-vindo ao jogo da média!');
+
+let quantidade = parseInt(prompt('Quantos números você deseja digitar?'));
+
+let soma = 0;
+
+for (let i = 1; i <= quantidade; i++) {
+  let numero = parseFloat(prompt('Digite o número ' + i + ':'));
+  soma += numero;
+}
+
+let media = soma / quantidade;
+
+alert('A média dos números é: ' + media);
